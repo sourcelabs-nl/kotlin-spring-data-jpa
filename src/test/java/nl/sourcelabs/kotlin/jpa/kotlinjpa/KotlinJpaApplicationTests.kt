df5c8bba58a5c52f2f93da8b0907f4707e97d9c4
Stephan Oudmaijer <soudmaijer@gmail.com>
2019-11-22 13:13:11 +0100

    Some explaination

--- src/test/java/nl/sourcelabs/kotlin/jpa/kotlinjpa/KotlinJpaApplicationTests.kt
+++ src/test/java/nl/sourcelabs/kotlin/jpa/kotlinjpa/KotlinJpaApplicationTests.kt
@@ -3,6 +3,9 @@ package nl.sourcelabs.kotlin.jpa.kotlinjpa
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import kotlin.test.assertEquals
+import kotlin.test.assertNotSame
+import kotlin.test.assertSame
 
 @DataJpaTest
 internal class KotlinJpaApplicationTests {
@@ -12,10 +15,18 @@ internal class KotlinJpaApplicationTests {
 
     @Test
     fun `invoking save after copy() should update and not insert new`() {
-        val myEntity = myRepository.save(MyEntity(prop1 = "Hello world"))
-        println(myEntity)
+        val myEntity = MyEntity(prop1 = "Hello world")
+        val myEntitySaved  = myRepository.save(myEntity)
+        assertSame(myEntity, myEntitySaved)
 
-        val myEntityCopy = myRepository.save(myEntity.copy(prop1 = "F#ck the world"))
-        println(myEntityCopy)
+        val myEntityCopy = myEntitySaved.copy(prop1 = "F#ck the world")
+        assertNotSame(myEntity, myEntityCopy)
+
+        // This one is SUPER tricky, because when saving this copy we also update the myEntity reference from above.
+        // Because the id is the same, JPA updates myEntity associated with the session/unit of work.
+        val myEntityCopySaved = myRepository.save(myEntityCopy)
+
+        assertNotSame(myEntityCopy, myEntityCopySaved) // <--------- They are NOT the same object reference!!!!!
+        assertEquals(myEntitySaved.id, myEntityCopySaved.id)
     }
 }
