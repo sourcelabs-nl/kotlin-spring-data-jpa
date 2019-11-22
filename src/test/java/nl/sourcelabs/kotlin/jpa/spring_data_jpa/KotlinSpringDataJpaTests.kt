88388455657e57fab9d059c3f1146c2948ba95ec
Stephan Oudmaijer <soudmaijer@gmail.com>
2019-11-22 13:25:29 +0100

    ....

--- src/test/java/nl/sourcelabs/kotlin/jpa/spring_data_jpa/KotlinSpringDataJpaTests.kt
+++ src/test/java/nl/sourcelabs/kotlin/jpa/spring_data_jpa/KotlinSpringDataJpaTests.kt
@@ -1,24 +1,35 @@
-package nl.sourcelabs.kotlin.jpa.kotlinjpa
+package nl.sourcelabs.kotlin.jpa.spring_data_jpa
 
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.jpa.repository.JpaRepository
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 import kotlin.test.assertEquals
 import kotlin.test.assertNotSame
 import kotlin.test.assertSame
 
+@Entity
+data class MyEntity(@Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null, val prop1: String)
+
+interface MyRepository : JpaRepository<MyEntity, Long>
+
 @DataJpaTest
-internal class KotlinJpaApplicationTests {
+class KotlinSpringDataJpaTests {
 
     @Autowired
     lateinit var myRepository: MyRepository
 
     @Test
-    fun `invoking save after copy() should update and not insert new`() {
+    fun `JPA and immutable data classes dont play nicely`() {
         val myEntity = MyEntity(prop1 = "Hello world")
-        val myEntitySaved  = myRepository.save(myEntity)
+        val myEntitySaved = myRepository.save(myEntity)
         assertSame(myEntity, myEntitySaved)
 
+        // Create a copy of myEntity (new instance).
         val myEntityCopy = myEntitySaved.copy(prop1 = ".... the world")
         assertNotSame(myEntity, myEntityCopy)
 
